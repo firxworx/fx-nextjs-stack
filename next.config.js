e62bd06a511234beab02c0435eaedcb66ace167d
Kevin Firko <kfirko@bitcurve.com>
2021-06-20 20:30:16 -0400

    strict mode + trailing slash in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+};
+
+// function style:
+// module.exports = (phase, { defaultConfig }) => {
+//   return {}
+// }
+
+// note: trailing slash helps facilitate static deployment especially on hosts such as s3
+// @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
