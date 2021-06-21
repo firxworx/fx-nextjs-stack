53361d814fa627eddb4e5853bf33f980531b7a37
Kevin Firko <kfirko@bitcurve.com>
2021-06-20 21:51:47 -0400

    use tailwind jit mode, require forms, aspect-ratio, and typography plugin - tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// @see https://tailwindcss.com/docs/configuration
+// for mode 'jit' refer to: https://tailwindcss.com/docs/just-in-time-mode
+
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/layout/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -17,5 +17,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
 }
