664b84a7e30cda135c938b8406498cfe08ff38da
Kevin Firko <kfirko@bitcurve.com>
2021-10-22 00:47:04 -0400

    cleanup aws-cdk stack - will deploy to aws with workable env's set

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -8,7 +8,7 @@ import * as codebuild from '@aws-cdk/aws-codebuild'
 export interface AwsCdkStackProps extends cdk.StackProps {
   project: {
     tag: string
-    domain: string
+    domain: string | undefined
   }
   app: {
     name: string
@@ -23,21 +23,17 @@ export interface AwsCdkStackProps extends cdk.StackProps {
     repository: string
     /** default branch name (typically 'main' or 'master') */
     defaultBranch: string
-    /** aws secrets manager secret id that corresponds to github personal access token */
-    tokenSecretId: string
+    /** identifier/name of the secret in aws secrets manager that contains an applicable github personal access token */
+    tokenSecret: string
   }
 }
-
-// ses, https redirect w/ route53 patterns, etc
-// https://github.com/tsmith512/tsmith-cdk/blob/3ffe29d88c2002443d87f13ee6d59c8a9e556f62/lib/tsmith-com-stack.ts
-
-// this guy gave up on amplify and went with codepipeline to s3
-// https://github.com/shariqanwar20/Virtual-Lolly-With-AWS/blob/e114994e98f961c69dbbd90b94b76af6b94f6d22/backend/lib/backend-stack.ts
 
 export class AwsCdkStack extends cdk.Stack {
   public readonly amplifyApp: amplify.App
 
   public readonly domain: amplify.Domain
+
+  public readonly basicAuth: amplify.BasicAuth | undefined
 
   public readonly branches: {
     readonly main: amplify.Branch
@@ -48,31 +44,29 @@ export class AwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: AwsCdkStackProps) {
     super(scope, id, props)
 
-    // note default amplify role may not have sufficient permissions to deploy ssr apps
-    // const role = new iam.Role(this, 'AmplifyRole', {
-    //   assumedBy: new iam.ServicePrincipal('amplify.amazonaws.com'),
-    // })
-    // role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')) // example
+    // best practice is for aws generate secure credentials: amplify.BasicAuth.fromGeneratedPassword('username')
+    this.basicAuth = amplify.BasicAuth.fromCredentials('preview', cdk.SecretValue.plainText('fx-nextjs-stack'))
 
     this.amplifyApp = new amplify.App(this, 'AmplifyApp', {
       appName: props.app.name,
       description: props.app.description,
       customRules: [
-        amplify.CustomRule.SINGLE_PAGE_APPLICATION_REDIRECT, // @todo research if right
         ...(props.app.customRules ? props.app.customRules : []),
-      ], // @todo redirect/rewrite rules // { source: .., target: .., status: .., }
+        amplify.CustomRule.SINGLE_PAGE_APPLICATION_REDIRECT,
+      ],
       environmentVariables: props.app.environmentVariables,
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: props.github.owner,
         repository: props.github.repository,
-        oauthToken: cdk.SecretValue.secretsManager(props.github.tokenSecretId),
-        // oauthToken: cdk.SecretValue.secretsManager('[Secret-Name]', {
-        //   jsonField: '[Secret-Key]',
+        oauthToken: cdk.SecretValue.secretsManager(props.github.tokenSecret),
+        // the following example shows how to access a value from a secret stored as json
+        // oauthToken: cdk.SecretValue.secretsManager('example/secret', {
+        //   jsonField: 'objectKey',
         // }),
       }),
       autoBranchCreation: {
         patterns: ['*'],
-        basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+        basicAuth: this.basicAuth,
       },
       autoBranchDeletion: true,
       buildSpec: codebuild.BuildSpec.fromObject({
@@ -83,16 +77,14 @@ export class AwsCdkStack extends cdk.Stack {
             frontend: {
               phases: {
                 preBuild: {
-                  // if required, add commands to install global packages
                   commands: ['yarn install'],
                 },
                 build: {
-                  // if required, add commands to run tests
                   commands: ['yarn build:static'],
                 },
               },
               artifacts: {
-                baseDirectory: '/dist', // '/', '.next' if not static
+                baseDirectory: '/dist', // use '.next' if not static
                 files: '**/*',
               },
               cache: {
@@ -104,64 +96,36 @@ export class AwsCdkStack extends cdk.Stack {
       }),
     })
 
-    // buildSpec: codebuild.BuildSpec.fromObjectToYaml({
-    //   version: "1.0",
-    //   frontend: {
-    //     phases: {
-    //       preBuild: {
-    //         commands: ["yarn install"],
-    //       },
-    //       build: {
-    //         commands: ["yarn run build"],
-    //       },
-    //     },
-    //     artifacts: {
-    //       baseDirectory: ".next",
-    //       files: "**/*",
-    //     },
-    //     cache: {
-    //       paths: "node_modules/**/*",
-    //     },
-    //   },
-    //   appRoot: "frontend",
-    // }),
-
     this.branches = {
       main: this.amplifyApp.addBranch('main'),
       dev: this.amplifyApp.addBranch('dev', {
-        basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+        basicAuth: this.basicAuth,
       }),
       staging: this.amplifyApp.addBranch('staging', {
-        basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+        basicAuth: this.basicAuth,
       }),
     }
 
-    // this.domain = this.amplifyApp.addDomain(props.project.domain)
-
-    this.domain = this.amplifyApp.addDomain(props.project.domain, {
-      // enableAutoSubdomain: true, // automatically create subdomains for registered branches
-      // autoSubdomainCreationPatterns: ['*', 'pr*'], // patterns for branches that should automatically create subdomains
-      // subDomains: [
-      //   {
-      //     branch: this.branches.dev,
-      //     prefix: 'dev',
-      //   },
-      //   {
-      //     branch: this.branches.staging,
-      //     prefix: 'staging',
-      //   },
-      //   {
-      //     branch: this.branches.main,
-      //     prefix: '',
-      //   },
-      // ],
-    })
-
-    this.domain.mapSubDomain(this.branches.dev, 'dev')
-    this.domain.mapSubDomain(this.branches.staging, 'staging')
-
-    // map main/master branch to domain root
-    this.domain.mapRoot(this.branches.main)
-    this.domain.mapSubDomain(this.branches.main, 'www') // @todo - or addSubdomain()??
+    if (props.project.domain) {
+      this.domain = this.amplifyApp.addDomain(props.project.domain, {
+        // enableAutoSubdomain: true, // automatically create subdomains for registered branches
+        // autoSubdomainCreationPatterns: ['*', 'pr*'], // patterns for branches that should automatically create subdomains
+        domainName: props.project.domain,
+        subDomains: [
+          {
+            branch: this.branches.dev,
+            prefix: 'dev',
+          },
+          {
+            branch: this.branches.staging,
+            prefix: 'staging',
+          },
+          {
+            branch: this.branches.main,
+            prefix: 'www',
+          },
+        ],
+      })
+    }
   }
 }
